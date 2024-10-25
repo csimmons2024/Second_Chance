4f6d679366962322ecb81772de4fa85cb5acea9e
Riviera Sperduto <riviera.t.sperduto13@gmail.com>
2024-10-25 09:51:08 -0400

    Changes to try to add Andrew's stuff in.... begrudgingly......

--- src/states/levels/level2_1.py
+++ src/states/levels/level2_1.py
@@ -3,7 +3,7 @@ from .level import Level
 
 class Level2_1(Level):
     def __init__(self):
-        imgArr = [f"plx-red-1.jpg"]
+        imgArr = [f"plx-{i}.png" for i in range(1, 6)]
         super().__init__(
             level=2,
             music_file="levelmusic.mp3",
@@ -12,6 +12,6 @@ class Level2_1(Level):
 
     def add_portal(self):
         # Add portal at specific coordinates
-        self.portal = Portal(2500, 700, "assets/backgrounds/portal.png", 150, 150)
+        self.portal = Portal(3500, 400, "assets/backgrounds/portal.png", 150, 150)
         self.portals.add(self.portal)
         self.objects.add(self.portals)
