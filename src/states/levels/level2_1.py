1ddde3580190b6899ce122cb0e2b61dcf4a2be63
Riviera Sperduto <riviera.t.sperduto13@gmail.com>
2024-10-25 10:24:52 -0400

    Everyone's shit works properly now....

--- src/states/levels/level2_1.py
+++ src/states/levels/level2_1.py
@@ -3,7 +3,7 @@ from .level import Level
 
 class Level2_1(Level):
     def __init__(self):
-        imgArr = [f"plx-{i}.png" for i in range(1, 6)]
+        imgArr = [f"plx-red-1.png"]
         super().__init__(
             level=2,
             music_file="levelmusic.mp3",
